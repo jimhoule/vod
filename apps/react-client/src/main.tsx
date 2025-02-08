9575bcb9c2a67f3ae01cead5a237c4b8fa3893f3
Jimmy Houle <jimhoule09@gmail.com>
2025-02-07 23:29:17 -0500

    Adding AuthProvider to app

--- apps/react-client/src/main.tsx
+++ apps/react-client/src/main.tsx
@@ -1,10 +1,13 @@
 import '@repo/tailwind-config/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { AuthProvider } from './providers/AuthProvider.tsx';
 import { RoutesProvider } from './providers/RoutesProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RoutesProvider />
+		<AuthProvider>
+			<RoutesProvider />
+		</AuthProvider>
 	</StrictMode>,
 );
