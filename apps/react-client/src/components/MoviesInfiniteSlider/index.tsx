bee03bac85d6d97c9fd5895c92d065432dcff49c
Jimmy Houle <jimhoule09@gmail.com>
2025-04-29 21:14:29 -0400

    creation of MoviesSlider components

---
+++ apps/react-client/src/components/MoviesInfiniteSlider/index.tsx
@@ -0,0 +1,67 @@
+import { Slider } from '@repo/ui/components/Slider';
+import { MoviesInfiniteSliderItem } from '../MoviesInfiniteSliderItem';
+
+type Movie = { id: string; name: string };
+
+type MoviesSliderProps = {
+	movies: Movie[];
+	slideItemsCount: number;
+};
+
+export function MoviesInfiniteSlider({ movies, slideItemsCount }: MoviesSliderProps) {
+	return (
+		<Slider.Infinite
+			items={movies}
+			slideItemsCount={slideItemsCount}
+			wasOneItemHoveredIdleTime={400}
+		>
+			<Slider.InfiniteIndicatorsContainer className=''>
+				{({ indicator, isCurrentSlide }) => {
+					const opacity = !isCurrentSlide ? 'opacity-50' : '';
+
+					return (
+						<div
+							key={indicator.index}
+							className={`h-[4%] w-[1%] bg-white ${opacity}`}
+						/>
+					);
+				}}
+			</Slider.InfiniteIndicatorsContainer>
+
+			<Slider.InfiniteFloatingControlsContainer>
+				<Slider.InfiniteItemsContainer<Movie>>
+					{({
+						infinitItems,
+						currentSlideIndex,
+						wasOneItemHovered,
+						wasOneItemHoveredIdleTime,
+						setWasOneItemHovered,
+						clearWasOneItemHovered,
+					}) => {
+						const handleMouseEnter = (): void => {
+							setWasOneItemHovered(true);
+						};
+
+						const handleMouseLeave = (): void => {
+							clearWasOneItemHovered(false);
+						};
+
+						return infinitItems.map((infinitItem: Movie, index: number) => (
+							<MoviesInfiniteSliderItem
+								key={index}
+								movie={infinitItem}
+								index={index}
+								currentSlideIndex={currentSlideIndex}
+								slideItemsCount={slideItemsCount}
+								isHoveredIdleTime={wasOneItemHoveredIdleTime}
+								canScale={wasOneItemHovered}
+								onMouseEnter={handleMouseEnter}
+								onMouseLeave={handleMouseLeave}
+							/>
+						));
+					}}
+				</Slider.InfiniteItemsContainer>
+			</Slider.InfiniteFloatingControlsContainer>
+		</Slider.Infinite>
+	);
+}
---
+++ apps/react-client/src/components/MoviesInfiniteSliderItem/index.tsx
@@ -0,0 +1,62 @@
+import { useIdledState } from '@repo/ui/hooks/useIdledState';
+
+type Movie = { id: string; name: string };
+
+type MoviesSliderItemProps = {
+	movie: Movie;
+	index: number;
+	currentSlideIndex: number;
+	slideItemsCount: number;
+	isHoveredIdleTime: number;
+	canScale: boolean;
+	onMouseEnter: () => void;
+	onMouseLeave: () => void;
+};
+
+export function MoviesInfiniteSliderItem({
+	movie,
+	index,
+	currentSlideIndex,
+	slideItemsCount,
+	isHoveredIdleTime,
+	canScale,
+	onMouseEnter,
+	onMouseLeave,
+}: MoviesSliderItemProps) {
+	const [isHovered, setIsHovered, clearIsHovered] = useIdledState(false, isHoveredIdleTime);
+
+	const isCurrentSlideFirstMovie = index === currentSlideIndex * slideItemsCount;
+	const isCurrentSlideLastMovie =
+		index === currentSlideIndex * slideItemsCount + (slideItemsCount - 1);
+	const transformOrigin = isCurrentSlideFirstMovie
+		? 'origin-[0%_120%]'
+		: isCurrentSlideLastMovie
+			? 'origin-[100%_120%]'
+			: 'origin-[50%_120%]';
+	const scale = canScale ? 'hover:scale-150 hover:shadow-lg' : '';
+	const display = isHovered ? 'block' : 'hidden';
+
+	const handleMouseEnter = (): void => {
+		setIsHovered(true);
+		onMouseEnter();
+	};
+
+	const handleMouseLeave = (): void => {
+		clearIsHovered(false);
+		onMouseLeave();
+	};
+
+	return (
+		<div key={movie.id} className={`box-border shrink-0 grow-0 basis-1/5 p-1`}>
+			<div
+				className={`size-full cursor-pointer transition-all duration-200 ease-in-out ${scale} ${transformOrigin}`}
+				onMouseEnter={handleMouseEnter}
+				onMouseLeave={handleMouseLeave}
+			>
+				<p className='size-full rounded-md bg-green-200'>{movie.name} image</p>
+
+				<div className={`h-2/3 w-full rounded-b-md bg-gray-900 ${display}`}></div>
+			</div>
+		</div>
+	);
+}
