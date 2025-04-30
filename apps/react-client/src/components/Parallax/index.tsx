a82886b7b04af9e72be1d7a2e0e37da314de9f05
Jimmy Houle <jimhoule09@gmail.com>
2025-04-29 21:18:34 -0400

    removing tailwind plugin in prettier for incompatibility with tailwind eslint plugin

--- apps/react-client/src/components/Parallax/index.tsx
+++ apps/react-client/src/components/Parallax/index.tsx
@@ -3,7 +3,7 @@ export function Parallax() {
 	return (
 		<div className='h-1/2 w-full'>
 			{/* parallax effect */}
-			<div className='bg-blank-img h-1/2 w-full bg-[length:100%_50%] bg-fixed bg-center bg-no-repeat' />
+			<div className='h-1/2 w-full bg-blank-img bg-[length:100%_50%] bg-fixed bg-center bg-no-repeat' />
 
 			{/* Element */}
 			<div className='h-1/2 w-full bg-red-500 opacity-50' />
