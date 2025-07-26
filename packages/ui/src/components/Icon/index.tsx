7c1c031b95dc195f4e153d35f4396f0177b7ce12
Jimmy Houle <jimhoule09@gmail.com>
2025-07-26 19:35:24 -0400

    Adding chevron icon to Icon component

--- packages/ui/src/components/Icon/index.tsx
+++ packages/ui/src/components/Icon/index.tsx
@@ -1,18 +1,22 @@
 import {
+	ChevronDownIcon as OutlinedChevronDownIcon,
 	HandThumbDownIcon as OutlinedHandThumbDownIcon,
 	HandThumbUpIcon as OutlinedHandThumbUpIcon,
 	HeartIcon as OutlinedHeartIcon,
 } from '@heroicons/react/24/outline';
 import {
+	ChevronDownIcon as SolidChevronDownIcon,
 	HeartIcon as SolidHeartIcon,
 	PlayIcon as SolidPlayIcon,
 	PlusIcon as SolidPlusIcon,
 } from '@heroicons/react/24/solid';
 
 type IconName =
+	| 'outlined-chevron-down'
 	| 'outlined-hand-thumb-down'
 	| 'outlined-hand-thumb-up'
 	| 'outlined-heart'
+	| 'solid-chevron-down'
 	| 'solid-heart'
 	| 'solid-play'
 	| 'solid-plus';
@@ -21,10 +25,12 @@ type IconCompnentsMap = {
 };
 
 const iconComponentsMap: IconCompnentsMap = {
+	'outlined-chevron-down': OutlinedChevronDownIcon,
 	'outlined-heart': OutlinedHeartIcon,
 	'outlined-hand-thumb-down': OutlinedHandThumbDownIcon,
 	'outlined-hand-thumb-up': OutlinedHandThumbUpIcon,
 
+	'solid-chevron-down': SolidChevronDownIcon,
 	'solid-heart': SolidHeartIcon,
 	'solid-play': SolidPlayIcon,
 	'solid-plus': SolidPlusIcon,
