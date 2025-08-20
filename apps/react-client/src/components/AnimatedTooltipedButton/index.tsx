b2ddc281260d313ed6e5cfca1adc09bf27afeb27
Jimmy Houle <jimhoule09@gmail.com>
2025-08-20 14:48:10 -0400

    creation of reusable AnimatedTooltipedButton component

---
+++ apps/react-client/src/components/AnimatedTooltipedButton/index.tsx
@@ -0,0 +1,43 @@
+import { AnimatedTooltip } from '@repo/ui/components/AnimatedTooltip';
+import type { Children } from '@repo/ui/types/Children'
+
+type AnimatedTooltipedButtonProps = {
+	buttonClassName: string;
+	children: Children;
+	tooltipContent: string;
+	onClick: () => void
+}
+
+export function AnimatedTooltipedButton({
+	buttonClassName,
+	children,
+	tooltipContent,
+	onClick,
+}: AnimatedTooltipedButtonProps) {
+	return (
+		<AnimatedTooltip
+			delayDuration={0}
+			contentProps={{
+				className: 'bg-white px-6 py-2 text-2xl font-semibold',
+				sideOffset: 5,
+			}}
+			arrowProps={{
+				className: 'fill-white',
+				width: 30,
+				height: 15,
+			}}
+		>
+			{{
+				content: tooltipContent,
+				trigger: (
+					<button
+						className={buttonClassName}
+						onClick={onClick}
+					>
+						{children}
+					</button>
+				),
+			}}
+		</AnimatedTooltip>
+	);
+}
