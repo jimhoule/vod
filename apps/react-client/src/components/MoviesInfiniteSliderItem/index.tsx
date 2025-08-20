0def5acb51a3b35294af143feeb7e02f4e660330
Jimmy Houle <jimhoule09@gmail.com>
2025-08-20 14:56:30 -0400

    adding buttons to MoviesInfiniteSliderItem component

--- apps/react-client/src/components/MoviesInfiniteSliderItem/index.tsx
+++ apps/react-client/src/components/MoviesInfiniteSliderItem/index.tsx
@@ -1,5 +1,8 @@
-import { useIdledState } from '@repo/ui/hooks/useIdledState';
 import { Movie } from '@repo/models/movies/Movie';
+import { Icon } from '@repo/ui/components/Icon';
+import { useIdledState } from '@repo/ui/hooks/useIdledState';
+import { AnimatedTooltipedButton } from '../AnimatedTooltipedButton';
+import { LikeButtons } from '../LikeButtons';
 
 type MoviesSliderItemProps = {
 	movie: Movie;
@@ -54,7 +57,49 @@ export function MoviesInfiniteSliderItem({
 			>
 				<p className='size-full rounded-md bg-green-200'>{movie.title} image</p>
 
-				<div className={`h-2/3 w-full rounded-b-md bg-gray-900 ${display}`}></div>
+				<div className={`h-3/4 w-full rounded-b-md bg-gray-900 p-4 ${display}`}>
+					{/* Buttons */}
+					<div className='flex h-1/2 w-full items-center bg-red-300'>
+						{/* Left Buttons */}
+						<div className='flex h-full w-1/2 items-center gap-1'>
+							<button
+								className='rounded-full bg-white p-2'
+								onClick={(): void => {
+									alert('Play movie');
+								}}
+							>
+								<Icon name='solid-play' className='size-6 text-black' />
+							</button>
+
+							<AnimatedTooltipedButton
+								buttonClassName='rounded-full border-2 border-solid border-green-700 bg-black p-2 hover:border-white'
+								tooltipContent='Add to my list'
+								onClick={(): void => {
+									alert('adding to list');
+								}}
+							>
+								<Icon name='solid-plus' className='size-6 text-white' />
+							</AnimatedTooltipedButton>
+
+							<LikeButtons />
+						</div>
+
+						{/* Right Buttons */}
+						<div className='flex h-full w-1/2 items-center justify-end'>
+							<AnimatedTooltipedButton
+								buttonClassName='rounded-full border-2 border-solid border-green-700 bg-black p-2 hover:border-white'
+								tooltipContent='More Info'
+								onClick={(): void => {
+									alert('Showing item details');
+								}}
+							>
+								<Icon name='solid-chevron-down' className='size-6 text-white' />
+							</AnimatedTooltipedButton>
+						</div>
+					</div>
+
+					<div className='h-1/2 w-full bg-blue-300' />
+				</div>
 			</div>
 		</div>
 	);
