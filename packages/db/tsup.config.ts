be7bbd28dcd602ccc285b230dc599e3b0149b140
Jimmy Houle <jimhoule09@gmail.com>
2025-10-01 18:27:25 -0400

    adding tsup entry point for orm helpers in db package

--- packages/db/tsup.config.ts
+++ packages/db/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ['./src/db.ts', './src/schema/*.ts'],
+	entry: ['./src/db.ts', './src/orm.ts', './src/schema/*.ts'],
 	format: ['esm'],
 	outDir: './dist',
 	sourcemap: true,
